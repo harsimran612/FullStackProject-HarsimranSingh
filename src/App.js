d6fe36e858c78b619c07092b967b12743c9ba5f1
Harsimran Singh <91652361+harsimran612@users.noreply.github.com>
2022-07-14 14:40:02 -0700

    Initial basic setup commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = props => {
+
+    return (
+        <>
+            <h1>Hello from React Project!!</h1>         
+        </>
+    )
+}
+
+export default App
